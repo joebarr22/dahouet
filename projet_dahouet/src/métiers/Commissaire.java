6fe0981d8be8a31599b515ff19863e71cd836450
DL11 <DL11@Etudes21.afpa.local>
2015-04-22 13:55:09 +0200

    incluide java project dahouet

---
+++ projet_dahouet/src/métiers/Commissaire.java
@@ -0,0 +1,29 @@
+package métiers;
+
+
+public class Commissaire extends Personne {
+	
+	private String comité;
+	
+	
+	public Commissaire(String nom, String mail, String prénom, String comité){
+		
+		
+			super(nom, mail, prénom, comité, comité);
+			
+			this.comité = comité;
+	}
+	
+	public void affichage(){
+		super.affichage();
+		System.out.println("Comité" + comité);
+	}
+
+	public String getComité() {
+		return comité;
+	}
+
+	public void setComité(String comité) {
+		this.comité = comité;
+	}
+}
---
+++ projet_dahouet/src/métiers/Personne.java
@@ -0,0 +1,51 @@
+package métiers;
+
+public class Personne {
+
+	public String nom;
+	public String mail;
+	public String prénom;
+	
+	
+	public Personne(){
+		
+		
+	}
+	
+public Personne(String nom,String mail, String prénom, String téléphone, String adresse){
+	super();
+	this.nom = nom;
+	this.mail = mail;
+	this.prénom = prénom;
+}
+
+public String getNom() {
+	return nom;
+}
+
+public void setNom(String nom) {
+	this.nom = nom;
+}
+
+public String getMail() {
+	return mail;
+}
+
+public void setMail(String mail) {
+	this.mail = mail;
+}
+
+public String getPrénom() {
+	return prénom;
+}
+
+public void setPrénom(String prénom) {
+	this.prénom = prénom;
+}
+public void affichage()
+{
+	System.out.println("nom"+" "+nom);
+	System.out.println("mail"+" "+mail);
+	System.out.println("prénom"+" "+prénom);
+}
+}
