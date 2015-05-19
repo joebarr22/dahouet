5a3aadce23003bd55633e30e90b59ef940753edb
joebarr22 <ddhallu@club-internet.fr>
2015-05-19 20:32:21 +0200

    +Voilier

---
+++ projet_dahouet/src/com/david/projet_dahouet/model/Voilier.java
@@ -0,0 +1,36 @@
+package com.david.projet_dahouet.model;
+
+public class Voilier {
+	
+	protected String nom;
+	protected double coef;
+	
+	
+	public Voilier(String nom, double coef) {
+		super();
+		this.nom = nom;
+		this.coef = coef;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public double getCoef() {
+		return coef;
+	}
+
+
+	public void setCoef(double coef) {
+		this.coef = coef;
+	}
+	
+	
+}
